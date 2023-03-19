a5d9ef80875a7f7a918a1e1b8b8d296c85b8fe68
tungnguyen9614 <tungnguyen9614@gmail.com>
2023-03-19 23:50:06 +0700

    fix

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,11 +1,7 @@
 import {combineReducers} from 'redux';
-import userReducer from './../user-management/duck/reducer';
 import {movieBookingReducer} from './../MovieBooking/duck/reducer';
 
 const rootReducer = combineReducers({
-    //quản lý các child reducer
-    //key: value
-    userReducer,//userReducer: userReducer
     movieBookingReducer,
 });
 
